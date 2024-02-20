0bc6f82dbff67ee8085af118fd19c1c2da01d3c7
blowtorch5 <pphilippot307@gmail.com>
2024-02-20 14:54:51 -0600

    Added Pagination to pokemons index

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -3,7 +3,7 @@ class PokemonsController < ApplicationController
 
   # GET /pokemons or /pokemons.json
   def index
-    @pokemons = Pokemon.all
+    @pokemons = Pokemon.paginate(page: params[:page], per_page: 100)
   end
 
   # GET /pokemons/1 or /pokemons/1.json
