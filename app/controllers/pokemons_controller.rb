5ec8bfdd5c876a269f0e34d5a8796d21cdeab037
blowtorch5 <pphilippot307@gmail.com>
2024-02-21 12:25:36 -0600

    Added images from pokeapi to pokemon pages

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -3,7 +3,7 @@ class PokemonsController < ApplicationController
 
   # GET /pokemons or /pokemons.json
   def index
-    @pokemons = Pokemon.paginate(page: params[:page], per_page: 100)
+    @pokemons = Pokemon.paginate(page: params[:page], per_page: 50)
   end
 
   # GET /pokemons/1 or /pokemons/1.json
