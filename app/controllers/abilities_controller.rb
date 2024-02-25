b163748a054606fcb67c2947fb8aa5af2e2a657f
blowtorch5 <pphilippot307@gmail.com>
2024-02-25 03:27:40 -0600

    Added pagination

--- app/controllers/abilities_controller.rb
+++ app/controllers/abilities_controller.rb
@@ -3,7 +3,7 @@ class AbilitiesController < ApplicationController
 
   # GET /abilities or /abilities.json
   def index
-    @abilities = Ability.all
+    @abilities = Ability.all.paginate(page: params[:page], per_page: 50)
   end
 
   # GET /abilities/1 or /abilities/1.json
